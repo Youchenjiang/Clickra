e727f850ee5a86d948239c19d2ee4de8cf1607f2
Youchen Jiang <1014308g@gmail.com>
2026-06-19 17:21:10 +0800

    feat(core): add FigureClipRegions to translation diagnostics
    1. Add FigureClipRegions property to TranslationPageDiagnostics class.

--- src/Clickra.Core/Models/TranslationPageDiagnostics.cs
+++ src/Clickra.Core/Models/TranslationPageDiagnostics.cs
@@ -9,6 +9,7 @@ namespace Clickra.Core.Models
         public int TableCount { get; set; }
         public List<TranslationRegionDiagnostics> TableMaskRegions { get; set; } = new();
         public List<TranslationRegionDiagnostics> DiagramMaskRegions { get; set; } = new();
+        public List<TranslationRegionDiagnostics> FigureClipRegions { get; set; } = new();
         public List<TranslationRegionDiagnostics> GrayPromptShadedRegions { get; set; } = new();
         public List<TranslationRegionDiagnostics> EffectiveGrayMaskRegions { get; set; } = new();
         public List<TranslationParagraphDiagnostics> Paragraphs { get; set; } = new();
